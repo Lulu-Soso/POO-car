369ca16ad685321389b85f8f4afc9bdcfd0d6031
Lulu-Soso <71519777+Lulu-Soso@users.noreply.github.com>
2021-04-05 21:20:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <?php
 
 require 'car.php';
+require 'Bicycle.php';
+
+$bike = new Bicycle("red");
+
+$bike->setvitesse = 21;
+var_dump($bike);
+
+echo $bike->forward();
+echo '<br> Vitesse du vélo : ' . $bike->setvitesse . ' km/h' . '<br>';
+echo $bike->brake();
+echo '<br> Vitesse du vélo : ' . $bike->setvitesse . ' km/h' . '<br>';
+echo $bike->brake();
 
 $br = "</br >";
 $car = new Car('black', 4,'plein');
